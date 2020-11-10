d3190f64c41b3d6016ab9f7b3aaa50f17f5526a0
Dain Brownlow <dain.brownlow@outlook.com>
2020-11-10 23:46:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_array[name]
+        binding.pry
+      end
+    end
+    final_array
+  end
+  
+  
+  
+    
+  
+  
+ 
+
+  
+  
 end
+
+
